fba4937c230633b5d719794719f94b50ef745a62
Nakk-i <nastyabond90@gmail.com>
2025-05-08 17:10:06 +0300

    add hw_10

--- backend/src/interfaces/user.interface.ts
+++ backend/src/interfaces/user.interface.ts
@@ -15,6 +15,13 @@ interface IUser extends IBase {
     avatar: string;
 }
 
+interface IUserQuery {
+    pageSize: number;
+    page: number;
+    search?: string;
+    order?: string;
+}
+
 type IUserCreateDTO = Pick<
     IUser,
     "email" | "password" | "name" | "surname" | "age"
@@ -22,4 +29,4 @@ type IUserCreateDTO = Pick<
 
 type IUserUpdateDTO = Pick<IUser, "name" | "surname" | "age">;
 
-export type { IUser, IUserCreateDTO, IUserUpdateDTO };
+export type { IUser, IUserCreateDTO, IUserQuery, IUserUpdateDTO };
--- backend/src/repositories/user.repository.ts
+++ backend/src/repositories/user.repository.ts
@@ -1,9 +1,50 @@
-import { IUser, IUserCreateDTO } from "../interfaces/user.interface";
+import { FilterQuery } from "mongoose";
+
+import {
+    IUser,
+    IUserCreateDTO,
+    IUserQuery,
+} from "../interfaces/user.interface";
 import { User } from "../models/user.model";
 
 class UserRepository {
-    public getAll(): Promise<IUser[]> {
-        return User.find();
+    public getAll(query: IUserQuery): Promise<IUser[]> {
+        // const skip = query.pageSize * (query.page - 1);
+        const filterObject: FilterQuery<IUser> = { isDeleted: false };
+
+        if (query.search) {
+            filterObject.$or = [
+                { name: { $regex: query.search, $options: "i" } },
+                { surname: { $regex: query.search, $options: "i" } },
+            ];
+        }
+
+        const orderObject = {};
+        if (query.order) {
+            if (query.order.startsWith("-")) {
+                orderObject[query.order.slice(1)] = -1;
+            } else {
+                orderObject[query.order] = 1;
+            }
+        }
+        return User.aggregate([
+            {
+                $match: filterObject,
+            },
+            {
+                $sort: orderObject,
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalItems: { $sum: 1 },
+                    data: { $push: "$$ROOT" },
+                },
+            },
+            {
+                $project: { _id: 0 },
+            },
+        ]);
     }
     public create(user: IUserCreateDTO): Promise<IUser> {
         return User.create(user);
@@ -20,11 +61,11 @@ class UserRepository {
     public getByEmail(email: string): Promise<IUser> {
         return User.findOne({ email });
     }
-    public bannedById(
+    public async bannedById(
         userId: string,
         dto: { isActive: boolean },
     ): Promise<IUser> {
-        return User.findByIdAndUpdate(
+        return await User.findByIdAndUpdate(
             userId,
             { isActive: dto.isActive },
             { new: true },
